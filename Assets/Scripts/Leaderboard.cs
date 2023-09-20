abace6833313636e11bd2a9b0bf205fdb794943b
DagdaIn <133874604+DagdaIn@users.noreply.github.com>
2023-09-20 14:10:10 +0100

    Made a prototype leaderboard system
    I have created a leaderboard method which involves sorting by time, by date, and uses delegates.

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,129 @@
+﻿#region includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+#endregion
+
+namespace NEALeaderboard
+{
+    #region structures
+    /// <summary>
+    /// Stores a date of when it was set, and stores a time
+    /// </summary>
+    struct LeaderboardData
+    {
+        public DateTime dateSet;
+        public TimeSpan timeTaken;
+    }
+    #endregion
+
+    internal class Leaderboard
+    {
+        private SortBy SortingMethod;
+        public SortBy sortingMethod
+        {
+            private get
+            {
+                if (SortingMethod != null)
+                {
+                    return SortingMethod;
+                }
+                else
+                {
+                    return DefaultSort;
+                }
+            }
+            set
+            {
+                SortingMethod = value;
+            }
+        }
+                
+        #region constructors
+        public Leaderboard() 
+        {
+            // Nothing right now
+        }
+        #endregion
+
+        #region subroutines
+        /// <summary>
+        /// Appends a date and time to a file
+        /// </summary>
+        /// <param name="saveData">The date and time to be saved to the file</param>
+        /// <param name="filename">The name of the file to be saved to "x.txt"</param>
+        public void SaveTime(LeaderboardData saveData, string filename)
+        {
+            using(StreamWriter sw = new StreamWriter(filename, true))
+            {
+                sw.WriteLine($"{saveData.dateSet:yy/MM/dd} {saveData.timeTaken.Minutes}:{saveData.timeTaken.Seconds}");
+            }
+        }
+
+        /// <summary>
+        /// Reads the first date and time from a file
+        /// </summary>
+        /// <param name="filename">The name of the file to be read from "x.txt"</param>
+        /// <returns>A date and time as a LeaderboardData structure</returns>
+        public LeaderboardData LoadTime(string filename)
+        {
+            LeaderboardData data = new LeaderboardData();
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string temp = sr.ReadLine();
+                data.dateSet = DateTime.Parse(temp.Split(' ')[0]);
+                data.timeTaken = TimeSpan.Parse(temp.Split(' ')[1]);
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Writes a date and time to the console
+        /// </summary>
+        /// <param name="data">A date and time saved as a LeaderboardData structure</param>
+        public void LogTime(LeaderboardData data)
+        {
+            Console.WriteLine($"{data.dateSet:yy/MM/dd} {data.timeTaken}");
+        }
+
+        /// <summary>
+        /// Loads all the times saved in a given file
+        /// </summary>
+        /// <param name="filename">The name of the file to be read from "x.txt"</param>
+        /// <returns>A List of dates and times as LeaderboardData structures</returns>
+        public List<LeaderboardData> LoadAllTimes(string filename)
+        {
+            List<LeaderboardData> times = new List<LeaderboardData>();
+            LeaderboardData tempData;
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while (!sr.EndOfStream)
+                {
+                    tempData = new LeaderboardData();
+                    string temp = sr.ReadLine();
+                    tempData.dateSet = DateTime.Parse(temp.Split(' ')[0]);
+                    tempData.timeTaken = TimeSpan.Parse(temp.Split(' ')[1]);
+
+                    times.Add(tempData);
+                }
+            }
+
+            sortingMethod(ref times);
+
+            return times;
+        }
+
+        public delegate void SortBy(ref List<LeaderboardData> data);
+
+        public void DefaultSort(ref List<LeaderboardData> data)
+        {
+            // Maintains the current order
+        }
+        #endregion
+    }
+}
--- Assets/Scripts/Program.cs
+++ Assets/Scripts/Program.cs
@@ -1,146 +1,53 @@
-﻿using System;
+﻿#region includes
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
+#endregion
 
-namespace NEA_Saving_Test
+namespace NEALeaderboard
 {
     internal class Program
     {
-        /// <summary>
-        /// Testing the implementation of the subroutines
-        /// </summary>
-        /// <param name="args"></param>
+        #region Main
         static void Main(string[] args)
         {
-            while(true)
+            Leaderboard leaderboard = new Leaderboard();
+            leaderboard.sortingMethod = SortByTime;
+            List<LeaderboardData> times = leaderboard.LoadAllTimes("Test.txt");
+
+            foreach (LeaderboardData time in times)
             {
-                Dictionary<string, string> data = new Dictionary<string, string>();
-                Console.WriteLine("Enter a name:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Enter data:");
-                string toSave = Console.ReadLine();
-                Save(toSave, name, "test.txt");
-                Read("test.txt", out data);
-
-                WriteData(data);
-
-                Console.WriteLine("Would you like to delete data? ");
-                if (Console.ReadLine() == "y")
-                {
-                    Console.WriteLine("Enter the name: ");
-                    name = Console.ReadLine();
-                    DeleteData("test.txt", name);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Saves a name with data to a file
-        /// </summary>
-        /// <param name="toSave">The data to be saved</param>
-        /// <param name="name">The name used to reference the data</param>
-        /// <param name="filename">The name of the file, or the filepath</param>
-        static void Save(string toSave, string name, string filename)
-        {
-            Dictionary<string, string> data = new Dictionary<string, string>();
-
-            Read(filename, out data);
-
-            if (data.ContainsKey(name))
-            {
-                Console.WriteLine("Name already used!");
-                return;
+                leaderboard.LogTime(time);
             }
 
-            using (StreamWriter sw = new StreamWriter(filename, true))
-            {
-                sw.WriteLine($"{name} {toSave}");
-            }
-        }
-
-        /// <summary>
-        /// Reads the data from a file
-        /// </summary>
-        /// <param name="filename">The name of the file, or the filepath</param>
-        /// <param name="data">The name and data as a dictionary</param>
-        static void Read(string filename, out Dictionary<string, string> data)
-        {
-            data = new Dictionary<string, string>();
-            using (StreamReader sr = new StreamReader(filename))
-            {
-                while (!sr.EndOfStream)
-                {
-                    string s = sr.ReadLine();
-                    data.Add(s.Split(' ')[0], ParseData(s.Split(' ')));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Logs the contents of <paramref name="data"/> to the console
-        /// </summary>
-        /// <param name="data">The dictionary containing all names and data</param>
-        static void WriteData(Dictionary<string, string> data)
-        {
-            foreach (KeyValuePair<string, string> dataPiece in data)
-            {
-                Console.WriteLine($"{dataPiece.Key} {dataPiece.Value}");
-            }
             Console.ReadKey();
-            Console.Clear();
         }
 
         /// <summary>
-        /// Returns a string of all but the 0th element of <paramref name="data"/>
+        /// Sorts <paramref name="times"/> by timeTaken
         /// </summary>
-        /// <param name="data">A string array of the data</param>
-        /// <returns>A single string of the 1st to the last element of the data</returns>
-        static string ParseData(string[] data)
+        /// <param name="times">The list of leaderboard data taken by reference</param>
+        public static void SortByTime(ref List<LeaderboardData> times)
         {
-            string result = "";
-            for (int i = 1; i < data.Length; i++)
-            {
-                result = $"{result} {data[i]}";
-            }
+            LeaderboardData[] arrayTimes = times.ToArray();
+            Array.Sort(arrayTimes, (x, y) => x.timeTaken.CompareTo(y.timeTaken));
 
-            return result.Trim();
+            times = arrayTimes.ToList();
         }
 
         /// <summary>
-        /// Deletes a line of data
+        /// Sorts <paramref name="dates"/> by dateSet
         /// </summary>
-        /// <param name="filename">The name of the file, or the filepath</param>
-        /// <param name="name">The name associated with the data</param>
-        static void DeleteData(string filename, string name)
+        /// <param name="dates">The list of leaderboard data taken by reference</param>
+        public static void SortByDate(ref List<LeaderboardData> dates)
         {
-            Dictionary<string, string> data = new Dictionary<string, string>();
-            Read(filename, out data);
+            LeaderboardData[] arrayDates = dates.ToArray();
+            Array.Sort(arrayDates, (x, y) => x.dateSet.CompareTo(y.dateSet));
 
-            if (!data.ContainsKey(name))
-            {
-                Console.WriteLine();
-            }
-
-            string toWrite = "";
-            using (StreamReader sr = new StreamReader(filename))
-            {
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-                    if (line != null && line.Split(' ')[0] != name)
-                    {
-                        toWrite = $"{toWrite}{line}\n";
-                    }
-                }
-            }
-
-            using (StreamWriter sw = new StreamWriter(filename, false))
-            {
-                sw.Write(toWrite);
-            }
+            dates = arrayDates.ToList();
         }
+        #endregion
     }
 }
